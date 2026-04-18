d40e400a6ea5c5fe717abd128ecff0e6f1defc6f
Thyago Santos <thyagoamaraji1@gmail.com>
2026-04-18 13:49:20 -0300

    comando git para criar nova branch

--- script.sql
+++ script.sql
@@ -1,8 +1,13 @@
-CREATE DATABASE github;
+CREATE DATABASE treina_recife;
 
 CREATE TABLE tb_alunos ([
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
+    nome VARCHAR(50) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
     data_nascimento DATE NOT NULL
 ]);
+
+INSERT INTO treina_recife.tb_alunos 
+(id, nome, email, data_nascimento) 
+VALUES
+(1, 'João Silva', 'joao.silva@example.com', TIMESTAMP '2000-01-01');
